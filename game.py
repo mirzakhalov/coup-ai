108d553ad73483b18599d77be3984e117c3c6cd8
Jamshidbek Mirzakhalov <mirzakhalov@techs-MacBook-Pro-2.local>
2019-09-02 19:48:48 -0400

    first commit

---
+++ game.py
@@ -0,0 +1,25 @@
+
+
+class Game():
+
+    deck = []
+    players = []
+    round_count = 0
+    active_player = None
+    alive_count = 5 
+
+
+
+    def init():
+
+
+
+    def challenge():
+
+
+
+    def do_action():
+
+
+    
+    def reset():
